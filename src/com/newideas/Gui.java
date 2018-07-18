6ce1baa8190669637d4d51c7d688674ff069a954
Cody <Cody@CODYSTOWER>
2018-07-17 20:19:10 -0400

    learning how to make guis
    following a few guides to get a better feel for what i am doing

--- src/com/newideas/Gui.java
+++ src/com/newideas/Gui.java
@@ -1,23 +1,61 @@
 package com.newideas;
 
-import java.awt.Button;
-import java.awt.Frame;
-import java.awt.Panel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Gui extends Frame
+public class Gui extends JFrame
 {
-	/*
-	 * Default Constructor takes no parameters and does initial setup
-	 */
+	private JTextField email_;
+	private JTextField password_;
+	private int sum = 0;
+
 	public Gui()
 	{
-		Panel pnl = new Panel();          // Panel is a container
-		Button btn = new Button("Press"); // Button is a component
-		pnl.add(btn);                     // The Panel container adds a Button component
+		Container cp = getContentPane();
+		cp.setLayout(new GridLayout(2, 2, 5, 5));
+		
+		new GridLayout();
+
+		cp.add(new JLabel("Email: "));
+		email_ = new JTextField(10);
+		cp.add(email_);
+		cp.add(new JLabel("Password: "));
+		password_ = new JTextField(10);
+		cp.add(password_);
+
+		// Allocate an anonymous instance of an anonymous inner class that
+		// implements ActionListener as ActionEvent listener
+		email_.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent evt)
+			{
+				// Get the String entered into the input TextField, convert to int
+				int numberIn = Integer.parseInt(email_.getText());
+				sum += numberIn; // accumulate numbers entered into sum
+				email_.setText(""); // clear input TextField
+				password_.setText(sum + ""); // display sum on the output TextField
+			}
+		});
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("New Ideas");
+		setSize(1280, 720);
+		setVisible(true);
 	}
 
-	public static void main(String[] a)
+	// The entry main() method
+	public static void main(String[] args)
 	{
-		new Gui();  
+		// Run the GUI construction in the Event-Dispatching thread for thread-safety
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				new Gui(); // Let the constructor do the job
+			}
+		});
 	}
 }
