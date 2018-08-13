e46b3e85f2868e6fd1906a03df63c11070b0faaf
Cody <Cody@CODYSTOWER>
2018-08-12 22:24:02 -0400

    api is complex but some progress seems to be being made
    trying out the tree model because streaming seems to be insane and classes didnt work out as hoped

--- src/com/poe/apireader/Test.java
+++ src/com/poe/apireader/Test.java
@@ -7,6 +7,11 @@ import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 
@@ -14,11 +19,34 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-		parsePage(getPage());
+		String jsonString = getPage();
 
-		System.out.println("made it here");
+		JsonParser parser = new JsonParser();
+		JsonElement rootNode = parser.parse(jsonString);
+
+		if (rootNode.isJsonObject())
+		{
+			JsonObject page = rootNode.getAsJsonObject();
+			
+			JsonElement nameNode = page.get("next_change_id");
+			System.out.println("next id: " + nameNode.getAsString());
+
+			JsonElement ageNode = page.get("stashes");
+			// System.out.println("Age: " + ageNode.getAsInt());
+			//
+			// JsonElement verifiedNode = details.get("verified");
+			// System.out.println("Verified: " + (verifiedNode.getAsBoolean() ? "Yes" :
+			// "No"));
+			// JsonArray marks = details.getAsJsonArray("marks");
+			//
+			// for (int i = 0; i < marks.size(); i++)
+			// {
+			// JsonPrimitive value = marks.get(i).getAsJsonPrimitive();
+			// System.out.print(value.getAsInt() + " ");
+			// }
+		}
 	}
-	
+
 	public static void parsePage(String json)
 	{
 		JsonReader reader = new JsonReader(new StringReader(json));
@@ -27,28 +55,64 @@ public class Test
 
 		try
 		{
-			reader.beginObject();
-			JsonToken token = reader.peek();
-			if (token.equals(JsonToken.NAME))
+			while (reader.hasNext())
 			{
-				// get the current token
-				String fieldname = reader.nextName();
+				JsonToken token = reader.peek();
 
-				if (fieldname.equals("next_change_id"))
+				if (token.equals(JsonToken.BEGIN_OBJECT))
 				{
-					nextId = reader.nextString();
+					reader.beginObject();
+					reader.endObject();
+				}
+				else if (token.equals(JsonToken.END_OBJECT))
+				{
+					reader.endObject();
+				}
+				else
+				{
+					if (token.equals(JsonToken.NAME))
+					{
+						// get the current token
+						String fieldname = reader.nextName();
+
+						if (fieldname.equals("next_change_id"))
+						{
+							nextId = reader.nextString();
+						}
+						else if (fieldname.equals("stashes"))
+						{
+							reader.beginArray();
+							while (reader.peek() != JsonToken.END_ARRAY)
+							{
+								reader.beginObject();
+								reader.nextString();
+								reader.nextBoolean();
+								reader.nextString();
+								reader.nextString();
+								reader.nextString();
+								reader.nextString();
+								reader.nextString();
+								reader.beginArray();
+								reader.endArray();
+								reader.endObject();
+							}
+
+							reader.endArray();
+						}
+					}
 				}
 			}
+
 		}
 		catch (IOException e)
 		{
 			System.err.println(e.getMessage());
 		}
-		
-		if(!nextId.equals(""))
+
+		if (!nextId.equals(""))
 		{
 			System.out.println(nextId);
-			
+
 			try
 			{
 				Thread.sleep(750);
@@ -57,7 +121,7 @@ public class Test
 			{
 				System.err.println(e.getMessage());
 			}
-			
+
 			parsePage(getPage(nextId));
 		}
 	}
