d508cb6748e4afd5f6a3d0067176e1a5a89e9cfd
Cody Ware <39965219+codyw124@users.noreply.github.com>
2018-09-12 22:33:26 -0400

    Forget eclipse and java. WPF is where it is at for programs with ui
    i am in the process of converting java to c# so that i can make my ui in wpf. it is going amazingly so far and has fixed the issue i was having with reading in the poe ninja apis current next change id

---
+++ Stash++/Stash++/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stash__
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            String currentNextChangeId = getCurrentChangeId();
+        }
+
+        private void button(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void addTab(String newTabName)
+        {
+            TabItem newTab = new TabItem();
+
+            newTab.Header = newTabName;
+
+            tabs.Items.Add(newTab);
+        }
+
+        private String getJsonFromUrl(String url)
+        {
+            return new WebClient().DownloadString(url);
+        }
+
+        private String getCurrentChangeId()
+        {
+            String url = "http://poe.ninja/api/Data/GetStats/";
+            String json = getJsonFromUrl(url);
+
+            JsonTextReader reader = new JsonTextReader(new StringReader(json));
+
+            bool keepLooking = true;
+
+            String nextChangeId = "";
+
+            do
+            {
+                reader.Read();
+
+                if(reader.Value != null && reader.Value.ToString() == "next_change_id")
+                {
+                    keepLooking = false;
+                    reader.Read();
+                    nextChangeId = reader.Value.ToString(); 
+                }
+            } while(keepLooking);
+
+            return nextChangeId;
+        }
+    }
+}
